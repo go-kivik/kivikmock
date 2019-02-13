065f1b41424a291af92c14f01e96e473f9bcc3cf
Jonathan Hall <flimzy@flimzy.com>
2019-02-13 15:46:01 +0100

    Move option comparison to common location

--- meets.go
+++ meets.go
@@ -1,12 +1,19 @@
 package kivikmock
 
-import "reflect"
+import (
+	"reflect"
+
+	"github.com/go-kivik/kivik"
+)
 
 func meets(a, e expectation) bool {
 	if reflect.TypeOf(a).Elem().Name() != reflect.TypeOf(e).Elem().Name() {
 		return false
 	}
 	if !dbMeetsExpectation(a.DB(), e.DB()) {
+		return false
+	}
+	if !optionsMeetExpectation(a.opts(), e.opts()) {
 		return false
 	}
 	return a.met(e)
@@ -17,4 +24,11 @@ func dbMeetsExpectation(a, e *MockDB) bool {
 		return true
 	}
 	return e.name == a.name && e.id == a.id
+}
+
+func optionsMeetExpectation(a, e kivik.Options) bool {
+	if e == nil {
+		return true
+	}
+	return reflect.DeepEqual(e, a)
 }
